68972c9ea7d0e878c31e67755fad327aedef4bd3
olivierlemoine <lemoineolivier35@gmail.com>
2019-03-16 14:32:49 +0100

    vec3 operation

--- src/utils/Vec3.ts
+++ src/utils/Vec3.ts
@@ -1,7 +1,81 @@
 export class Vec3 {
-    values: Float64Array;
+    values: Float64Array = new Float64Array(3);
 
-    constructor() {
-        this.values = new Float64Array(3);
+    public get x(): number {
+        return this.values[0];
+    }
+
+    public get y(): number {
+        return this.values[1];
+    }
+
+    public get z(): number {
+        return this.values[2];
+    }
+}
+
+export namespace Vec3 {
+    function add(vec3_1: Vec3, vec3_2: Vec3): Vec3;
+    function add(vec3: Vec3, n: number): Vec3;
+    function add(a: Vec3, b: Vec3 | number): Vec3 {
+        let res = new Vec3();
+        if (typeof b === 'number') {
+            res.values[0] = a.values[0] + b;
+            res.values[1] = a.values[1] + b;
+            res.values[2] = a.values[2] + b;
+        } else {
+            res.values[0] = a.values[0] + b.values[0];
+            res.values[1] = a.values[1] + b.values[1];
+            res.values[2] = a.values[2] + b.values[2];
+        }
+        return res;
+    }
+
+    function sub(vec3_1: Vec3, vec3_2: Vec3): Vec3;
+    function sub(vec3: Vec3, n: number): Vec3;
+    function sub(a: Vec3, b: Vec3 | number): Vec3 {
+        let res = new Vec3();
+        if (typeof b === 'number') {
+            res.values[0] = a.values[0] - b;
+            res.values[1] = a.values[1] - b;
+            res.values[2] = a.values[2] - b;
+        } else {
+            res.values[0] = a.values[0] - b.values[0];
+            res.values[1] = a.values[1] - b.values[1];
+            res.values[2] = a.values[2] - b.values[2];
+        }
+        return res;
+    }
+
+    function multiply(vec3_1: Vec3, vec3_2: Vec3): Vec3;
+    function multiply(vec3: Vec3, n: number): Vec3;
+    function multiply(a: Vec3, b: Vec3 | number): Vec3 {
+        let res = new Vec3();
+        if (typeof b === 'number') {
+            res.values[0] = a.values[0] * b;
+            res.values[1] = a.values[1] * b;
+            res.values[2] = a.values[2] * b;
+        } else {
+            res.values[0] = a.values[0] * b.values[0];
+            res.values[1] = a.values[1] * b.values[1];
+            res.values[2] = a.values[2] * b.values[2];
+        }
+        return res;
+    }
+
+    function divide(vec3_1: Vec3, vec3_2: Vec3): Vec3;
+    function divide(vec3: Vec3, n: number): Vec3;
+    function divide(a: Vec3, b: Vec3 | number): Vec3 {
+        let res = new Vec3();
+        if (typeof b === 'number') {
+            res.values[0] = a.values[0] / b;
+            res.values[1] = a.values[1] / b;
+            res.values[2] = a.values[2] / b;
+        } else {
+            res.values[0] = a.values[0] / b.values[0];
+            res.values[1] = a.values[1] / b.values[1];
+            res.values[2] = a.values[2] / b.values[2];
+        }
+        return res;
     }
 }
