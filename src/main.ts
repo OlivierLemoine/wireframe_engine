b8e01bfe6646426114e134ed87982758e9e0252d
olivierlemoine <lemoineolivier35@gmail.com>
2019-03-16 16:29:44 +0100

    test everything

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import * as engine from "./engine.js";
+
+let a = new engine.GameObject();
+let b = new engine.Renderer(() => {
+    let c = new engine.GameObject();
+})
+let d = new engine.GameObject();
+console.log(b);
